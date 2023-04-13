6e903e0302d14489974d95c46bd92245b5bcfd6b
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-04-13 14:35:25 +0700

    penambahan void input

--- LinearSearch_125/LinearSearch_125.cpp
+++ LinearSearch_125/LinearSearch_125.cpp
@@ -5,6 +5,18 @@ int arr[20];                        //array to be searched
 int n;                              //number of element in the array
 int i;                              //index of array element
 
+void input()
+{
+    while (true)
+    {
+        cout << "enter the number of element in the array : ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+    }
+}
 
 
 
