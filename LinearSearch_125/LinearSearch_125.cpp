206ffdeab134739b835b3ba94fa97c749fab19fa
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-04-13 14:56:28 +0700

    penambahan langkah no 5 algoritma

--- LinearSearch_125/LinearSearch_125.cpp
+++ LinearSearch_125/LinearSearch_125.cpp
@@ -43,7 +43,11 @@ void linearsearch()
         ctr = 0;
         for (i = 0; i < n; i++)                                     //langkah no 2,3,4 algoritma
         {
+            ctr++;
+            if (arr[i] == item)
+            {
 
+            }
         }
     }
 }
